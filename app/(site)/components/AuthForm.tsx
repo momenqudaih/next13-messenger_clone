b3a909f3aa171e0f2a62facbad02e8a79b181e4c
Momen Qudaih <momenqudaih1@gmail.com>
2023-08-15 09:38:41 +0300

    feat: implement google and github auth and normal Credentials auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,9 @@ import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type variant = 'LOGIN' | 'REGISTER';
 
@@ -37,17 +40,39 @@ const AuthForm = () => {
         setIsLoading(true);
 
         if (variant === 'REGISTER') {
-            // Register user axios
+            axios
+                .post('/api/register', data)
+                .catch(() => toast.error('Something went wrong!'))
+                .finally(() => setIsLoading(false));
         }
         if (variant === 'LOGIN') {
-            // Next Auth login
+            signIn('credentials', { ...data, redirect: false })
+                .then((callback) => {
+                    if (callback?.error) {
+                        toast.error('Invalid credentials!');
+                    }
+
+                    if (callback?.ok && !callback?.error) {
+                        toast.success('Logged in successfully!');
+                    }
+                })
+                .finally(() => setIsLoading(false));
         }
     };
 
     const socialAction = (action: string) => {
         setIsLoading(true);
+        signIn(action, { redirect: false })
+            .then((callback) => {
+                if (callback?.error) {
+                    toast.error('Invalid credentials!');
+                }
 
-        // NextAuth social login
+                if (callback?.ok && !callback?.error) {
+                    toast.success('Logged in successfully!');
+                }
+            })
+            .finally(() => setIsLoading(false));
     };
 
     return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import AuthContext from './context/AuthContext';
+// import ActiveStatus from './components/ActiveStatus';
+import ToasterContext from './context/ToasterContext';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-    title: 'Messenger Clone',
+export const metadata = {
+    title: 'Messenger',
     description: 'Messenger Clone',
 };
 
@@ -16,7 +15,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body>
+                <ToasterContext />
+                {children}
+            </body>
         </html>
     );
 }
