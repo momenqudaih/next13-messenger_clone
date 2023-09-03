abfa95302cb1f74ba7c20cb2c9f787a533caa33e
Momen Qudaih <momenqudaih1@gmail.com>
2023-09-03 15:59:34 +0300

    fix : fix active status to return null

--- app/components/ActiveStatus.tsx
+++ app/components/ActiveStatus.tsx
@@ -4,7 +4,7 @@ import useActiveChannel from '../hooks/useActiveChannel';
 
 const ActiveStatus = () => {
     useActiveChannel();
-    return <div>ActiveStatus</div>;
+    return null;
 };
 
 export default ActiveStatus;
